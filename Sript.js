71e1464ee5e28a39acce792492605ebfe4a89799
ItsJayKee <jaykeecamay2022@gmail.com>
2025-03-21 17:24:27 +0800

    Create Sript.js

---
+++ Sript.js
@@ -0,0 +1,18 @@
+        function scrollToHome() {
+            document.getElementById("main-container").scrollIntoView({ behavior: "smooth" });
+        }
+        function scrollToAbout() {
+            document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+        }
+        function scrollToSkills() {
+            document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
+        }
+        function scrollToServices() {
+            document.getElementById("services").scrollIntoView({ behavior: "smooth" });
+        }
+        function scrollToContact() {
+            document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+        }
+                            function scrollToContacts() {
+            document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+                            }
